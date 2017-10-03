43b8b0ceeb4feaead291949859cd837e9e8dd0cf
Heat Lai <HeatLai@users.noreply.github.com>
2017-10-03 00:45:57 -0500

    Update BitsFieldOptions.php

--- example/BitsFieldOptions.php
+++ example/BitsFieldOptions.php
@@ -13,13 +13,18 @@ $options = array(1, 2, 4, 8);
 
 # 檢查選擇了哪些選項
 foreach ($options as $option)
-{   
+{
     # 二進位交集
     $pick = $option & $input;
 
     # $pick有值代表包含此選項 加到選擇清單內
     if($pick) $chooses[] = $option;
 }
+
+/* or */
+
+// $picks = splitBitsToDecimalArray($input);
+// $chooses = array_intersect($options, $picks);
 
 # 執行選擇
 foreach ($chooses as $value)
@@ -42,4 +47,33 @@ foreach ($chooses as $value)
             echo "choose unknown".PHP_EOL;
             break;
     }
+}
+
+exit;
+
+# Bits 轉 Decimal Array
+function splitBitsToDecimalArray($int)
+{
+    # 轉二進位字串
+    $binary = base_convert($int, 10, 2);
+
+    # 二進位字串長度
+    $length = strlen($binary);
+
+    $result = array();
+    # 計算每位數 2^n 從最大算到最小
+    for ($i = 0; $i < $length; $i++)
+    {
+        # 目前位數 0 or 1
+        $number = substr($binary, $i, 1);
+
+        # 目前位數為1 => 加入到結果
+        if($number)
+        {
+            # 從前面插入到結果
+            array_unshift($result, pow(2, ($length - 1) - $i) );
+        }
+    }
+
+    return $result;
 }
